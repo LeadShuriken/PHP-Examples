c130ab18e2580286f8f8bfe18b52ae3865721e97
Deian Mishev <d.s.mishev@gmail.com>
2017-01-22 20:29:21 +0200

    Update

--- SignInWithSteam/index.php
+++ SignInWithSteam/index.php
@@ -11,34 +11,28 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        include("SteamAuth.php");
-        $auth = new SteamAuth();
+        include("src/SignInWithSteam.php");
+        $signIn = new SignInWithSteam();
 
-        $auth->SetOnLoginCallback(function($steamid) {
+        $signIn->LoginCallback(function($steamid) {
             return true;
         });
 
-        $auth->SetOnLoginFailedCallback(function() {
+        $signIn->LogoutCallback(function($steamid) {
             return true;
         });
 
-        $auth->SetOnLogoutCallback(function($steamid) {
-            return true;
-        });
-
-        $auth->Init();
+        $signIn->Init();
 
         if (isset($_POST['logout'])) {
-            $auth->Logout(); // The logout function also refreshes the page
-        }
+            $signIn->Logout();
 
-        if ($auth->IsUserLoggedIn()) {
-            echo "Your SteamID is " . $auth->SteamID . "<br/>";
-            // We use POST to logout so people can't embed images to the logout function and annoy people.
+        if ($signIn->IsUserLoggedIn()) {
+            echo "Your SteamTokenID is " . $signIn->GetStemToken() . "<br/>";
             echo "<form method=\"POST\"><input type=\"submit\" name=\"logout\" value=\"Logout\" /></form>";
         } else {
             // Display login button
-            echo "<a href=\"" . $auth->GetLoginURL() . "\"><img src=\"assets/sits_large_noborder.png\" alt=\"Sign in through Steam\" /></a>";
+            echo "<a href=\"" . $signIn->GetLoginURL() . "\"><img src=\"img/sits.png\" /></a>";
         }
         ?>
     </body>
---
+++ SignInWithSteam/src/SignInWithSteam.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+include("openid.php");
+
+/**
+ * Description of SteamAuth
+ *
+ * @author dsmis
+ */
+if (session_id() == '') {
+    session_start();
+}
+
+class SignInWithSteam {
+
+    private $OpenID;
+    private $OnLoginCallback;
+    private $OnLogoutCallback;
+    private $SteamTokenID;
+
+    public function __construct($ServerName = 'DEFAULT') {
+        if ($ServerName = 'DEFAULT')
+            $ServerName = $_SERVER['SERVER_NAME'];
+
+        $this->OpenID = new LightOpenID($ServerName);
+        $this->OpenID->identity = 'http://steamcommunity.com/openid';
+    }
+
+    public function __call($closure, $args) {
+        return call_user_func_array($this->$closure, $args);
+    }
+
+    public function Init() {
+        if ($this->IsUserLoggedIn()) {
+            $this->SteamTokenID = $_SESSION['steamid'];
+            return;
+        } else if ($this->OpenID->validate()) {
+            $this->SteamTokenID = basename($this->OpenID->identity);
+            if ($this->OnLoginCallback($this->SteamTokenID)) {
+                $_SESSION['steamid'] = $this->SteamTokenID;
+            }
+        }
+    }
+
+    public function IsUserLoggedIn() {
+        return (isset($_SESSION['steamid']) && (strpos($_SESSION['steamid'], "7656") === 0));
+    }
+
+    public function RedirectLogin() {
+        header("Location: " . $this->GetLoginURL());
+    }
+
+    public function GetLoginURL() {
+        return $this->OpenID->authUrl();
+    }
+
+    public function GetStemToken() {
+        return $this->SteamTokenID;
+    }
+
+    public function Logout() {
+        $this->OnLogoutCallback($this->SteamTokenID);
+        unset($_SESSION['steamid']);
+        header("http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    }
+
+    public function LoginCallback($OnLoginCallback) {
+        $this->OnLoginCallback = $OnLoginCallback;
+    }
+
+    public function LogoutCallback($OnLogoutCallback) {
+        $this->OnLogoutCallback = $OnLogoutCallback;
+    }
+}
